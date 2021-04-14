ce092534676b456155e5dd88891194599e3bf575
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-04-14 21:37:30 +0530

    Fixed app path

--- app-loader.py
+++ app-loader.py
@@ -6,40 +6,50 @@ def menu():
         print("2: Add App")
         print("3: Update App")    
         print("4: Remove App")
-        choice=int(input("Enter your choice:"))
+        print("5: Exit")
+        choice=int(input("Enter your choice : "))
         if choice == 1:
             run()
         elif choice == 2:
             add()
-        elif choice == 3:
-            update()
-        elif choice == 4:
-            remove()
+        #elif choice == 3:
+            #update()
+        #elif choice == 4:
+            #remove()
+        #elif choice == 5:
+            #quit()
         else:
             print("Invalid Input")
             menu()
 
-
 def run():
-    if os.path.isfile('app-paths.txt'):
-        if os.path.getsize('app-paths.txt') > 0:
+    if os.path.isfile('app-paths'):
+        if os.path.getsize('app-paths') > 0:
             print ("File exist and not empty")
-            f = open("app-paths.txt")
+            f = open("app-paths")
         print ("File empty!")
     print("File not exist")
 
 def add():
     app_path=(input("Add app path : "))
-    exists = False
-    if os.path.isfile('app-paths.txt'):
-        exists = True
+    if os.path.isfile("app-paths"):
+        with open("app-paths", "a+") as file_append:
+            # Move read cursor to the start of file.
+            file_append.seek(0)
+            # If file is not empty then append '\n'
+            data = file_append.read(100)
+            if len(data) > 0 :
+                file_append.write("\n")
+            # Append text at the end of file
+            file_append.write(app_path)
     else:
-        exists = False
+        f= open("app-paths","w+")
+        f.write(app_path)
     
-        with open(filename, "app-paths.text")
-def update():
 
-def remove():
+#def update():
+
+#def remove():
 
 def main():
     menu()
