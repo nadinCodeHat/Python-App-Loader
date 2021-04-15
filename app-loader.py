ad8d1696de80d7f7bfff00228d21ca3dd300b8c5
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-04-15 11:26:16 +0530

    run() completed

--- app-loader.py
+++ app-loader.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 from fileinput import filename
 
 def menu():
@@ -25,27 +25,33 @@ def menu():
 def run():
     if os.path.isfile('app-paths'):
         if os.path.getsize('app-paths') > 0:
-            print ("File exist and not empty")
-            f = open("app-paths")
-        print ("File empty!")
-    print("File not exist")
+            count = 0
+            with open("app-paths") as file_read:
+                while True:
+                    count += 1
+                    line = file_read.readline()
+            
+                    if not line:
+                        break
+                    os.startfile(line.strip())
+                file_read.close()
+        else: print ("File empty!")
+    else: print("File not exist")
 
 def add():
     app_path=(input("Add app path : "))
     if os.path.isfile("app-paths"):
         with open("app-paths", "a+") as file_append:
-            # Move read cursor to the start of file.
             file_append.seek(0)
-            # If file is not empty then append '\n'
             data = file_append.read(100)
             if len(data) > 0 :
                 file_append.write("\n")
-            # Append text at the end of file
             file_append.write(app_path)
+        file_append.close()
     else:
-        f= open("app-paths","w+")
-        f.write(app_path)
-    
+        with open("app-paths","w+") as file_write:
+            file_write.write(app_path)
+        file_write.close()
 
 #def update():
 
